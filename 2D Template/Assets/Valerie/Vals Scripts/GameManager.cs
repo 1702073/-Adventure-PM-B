792d6af4b40f6e55b5c10ec19caf1e2889b9e068
MUKILTEO\1725631 <1725631@mukilteo.wednet.edu>
2025-10-08 13:28:13 -0700

    Worked on SLIDE Puzzel

---
+++ 2D Template/Assets/Valerie/Vals Scripts/GameManager.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private Transform gameTransform;
+    [SerializeField] private Transform piecePrefab;
+
+    private List<Transform> pieces;
+    private int emptyLoaction;
+    private int size;
+
+    // Create the game setup with x size pieces.
+
+    private void CreateGamePieces(float gapThickness)
+    {
+        // This is the width of each tile.
+        float width = 1 / (float)size;
+        for (int row = 0; row < size; row++)
+        {
+            for (int col = 0; col < size; col++)
+            {
+                Transform piece = Instantiate(piecePrefab, gameTransform);
+                pieces.Add(piece);
+                // Pieces will be in a gameboard going from -1 to +1
+                piece.localPosition = new Vector3(-1 + (2 * width * col) + width,
+                                                  +1 - (2 * width * row) - width,
+                                                  0);
+                piece.localScale = ((2 * width) - gapThickness) * Vector3.one;
+                piece.name = $"{(row * size) + col}";
+                // We want an empty space in the bottom right.
+                if ((row == size - 1) && (col == size - 1))
+                {
+                    emptyLoaction = (size * size) - 1;
+                    piece.gameObject.SetActive(false);
+                } else { 
+                // We want to map the UV coordinates appropriatly, they are 0->1.
+                float gap = gapThickness / 2;
+                Mesh mesh = piece.GetComponent<MeshFilter>().mesh;
+                Vector2[] uv = new Vector2[4];
+                    // UV coord order: (0, 1), (1, 1), (0, 0), (1, 0)
+                    uv[0] = new Vector2((width * col) + gap, 1 - ((width * (row + 1)) - gap));
+                    uv[1] = new Vector2((width * (col + 1)) - gap, 1 - ((width * (row + 1)) - gap));
+                    uv[2] = new Vector2((width * col) + gap, 1 - ((width *row) + gap));
+                    uv[3] = new Vector2((width * (col + 1)) - gap, 1 - ((width * row) + gap));
+                    // Assign our new UVs to the mesh.
+                    mesh.uv = uv;
+                }
+            }
+        }
+    }
+
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        size = 3;
+        CreateGamePieces(0.01f);
+    }
+}
